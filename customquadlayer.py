7e5fde631e33e0afb753258c67ee02b0049f4438
C Guo <bunjuku8@gmail.com>
2024-10-02 02:42:31 -0700

    Added paragraph column

--- customquadlayer.py
+++ customquadlayer.py
@@ -17,21 +17,44 @@ from sklearn import datasets
 from sklearn.linear_model import LogisticRegression
 
 from sklearn.svm import LinearSVC
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
 from sklearn.pipeline import make_pipeline
+from sklearn.ensemble import StackingRegressor
 from sklearn.ensemble import StackingClassifier
 from sklearn.model_selection import train_test_split
-
+from sklearn import compose, linear_model, metrics, pipeline, preprocessing
 import matplotlib.pyplot as plt
 import numpy as np
 
+class MyStackingRegressor(StackingRegressor):
+
+    # overwrite the inherited method without the check
+    def _validate_estimators(self):
+        if self.estimators is None or len(self.estimators) == 0:
+            raise ValueError(
+                "Invalid 'estimators' attribute, 'estimators' should be a list"
+                " of (string, estimator) tuples."
+            )
+        names, estimators = zip(*self.estimators)
+        self._validate_names(names)
+
+        has_estimator = any(est != "drop" for est in estimators)
+        if not has_estimator:
+            raise ValueError(
+                "All estimators are dropped. At least one is required "
+                "to be an estimator."
+            )
+        return names, estimators
+
 data = pd.read_csv("streaming.csv")
-data = data.drop(['version'], axis=1)
-#TODO: Automate data cleaning
-# - Regex for alpha after newline
-# - Find multistage and make something else
 data.stage = data.stage.apply(pd.to_numeric,errors='coerce')
+data.paragraph = data.paragraph.apply(lambda x: len(x))
 data = data.dropna()
+
+scaler = MinMaxScaler()
+
+# Fit and transform the data
+data = pd.DataFrame(scaler.fit_transform(data), columns=data.columns)
 
 print(data)
 
@@ -40,7 +63,8 @@ y = data.loc[:,"stage"].astype(int)
 y.fillna(0)
 
 X = data[["scarcity", "nonuniform_progress", "performance_constraints", 
-"user heterogeneity", "cognitive", "external", "internal", "coordination", "technical", "demand"]]
+"user heterogeneity", "cognitive", "external", "internal", "coordination", "technical", "demand", "paragraph"]]
+
 
 #replace nans with means
 # Splitting arrays or matrices into random train and test subsets
@@ -50,17 +74,24 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20)
 
 estimators = [
     ('rf', RandomForestClassifier(n_estimators=10, random_state=42)),
-    ('svr', make_pipeline(StandardScaler(),
-                          LinearSVC(random_state=42)))
+    ('svr', make_pipeline(PolynomialFeatures(degree=2),
+                        MinMaxScaler(),
+                        LinearSVC(random_state=42)))
 ]
-clf = StackingClassifier(
-    estimators=estimators, final_estimator=LogisticRegression()
-)
+#mnlr = linear_model.LogisticRegression(penalty='l2')
+ #multinomial logistic regression
+# poly = PolynomialFeatures(degree=2)
 
-
- 
+# Construct a TransformedTargetRegressor using this pipeline
+# ** So far the set-up has been standard **
+# regr = compose.TransformedTargetRegressor(regressor=mnlr, transformer=poly)
 # # Training the model on the training dataset
 # # fit function is used to train the model using the training sets as parameters
+
+clf = StackingClassifier(
+    estimators=estimators, final_estimator=linear_model.LogisticRegression(penalty='l2')
+)
+
 clf.fit(X_train, y_train)
 
 # z_train = y_train
