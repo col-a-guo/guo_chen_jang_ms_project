504f425a7ccfa249989ca52a901857b4c80c0093
Collin A Guo <bunjuku8@gmail.com>
2024-10-03 05:50:24 -0700

    try RFE

--- feature_importance_search.py
+++ feature_importance_search.py
@@ -15,13 +15,17 @@ from sklearn.preprocessing import PolynomialFeatures, MinMaxScaler
 from sklearn import metrics  
 from sklearn import datasets
 from sklearn.linear_model import LogisticRegression
-
-from sklearn.model_selection import train_test_split
-
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import RepeatedStratifiedKFold
+from sklearn.feature_selection import RFE
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split, cross_validate
+from sklearn.preprocessing import LabelEncoder
 import matplotlib.pyplot as plt
 import numpy as np
 
-data = pd.read_csv("streaming.csv")
+data = pd.read_csv("guo_chen_jang_ms_project//streaming.csv")
 #TODO: Automate data cleaning
 # - Regex for alpha after newline: [^0-9^,^"]+ and then ctrl alt enter
 # - Find multistage and make something else
@@ -76,22 +80,29 @@ y_pred = clf.predict(X_test)
 
 importances = clf.feature_importances_
  
+
 # using metrics module for accuracy calculation
 print("ACCURACY OF THE MODEL:", metrics.accuracy_score(y_test, y_pred))
 
-# Sort feature importances in descending order
-indices = np.argsort(importances)[::-1]
+label = LabelEncoder()
+data['stage'] = label.fit_transform(data['stage'])
+data['stage'].value_counts()
 
-# Rearrange feature names so they match the sorted feature importances
 
-names = [X.columns[i] for i in indices]
+#RFE feature importance
+rfe = RFE(estimator=RandomForestClassifier(), n_features_to_select=4)
+model = RandomForestClassifier() # instantiate a model
+pipeline = Pipeline(steps=[('Feature Selection', rfe), ('Model', model)])
 
-# Create plot
-plt.figure(figsize=(10, 6))
-plt.title("Feature Importances")
-plt.bar(range(X.shape[1]), importances[indices])
-plt.xticks(range(X.shape[1]), names, rotation=90)
-plt.xlabel("Features")
-plt.ylabel("Importance")
-plt.subplots_adjust(bottom=0.4)
-plt.show()
+# evaluate the model
+cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=5, random_state=1)
+results = cross_validate(pipeline, X, y, scoring='accuracy', cv=cv, return_estimator=True)
+n_scores = cross_validate(pipeline, X, y, scoring='accuracy', cv=cv, n_jobs=1)
+
+for iter, pipe in enumerate(results['estimator']):
+    print(f'Iteration no: {iter}')
+    for i in range(X.shape[1]):
+        print('Column: %s, Selected %s, Rank: %d' %
+            (X.columns[i], pipe['Feature Selection'].support_[i], pipe['Feature Selection'].ranking_[i]))
+        
+print(n_scores)
