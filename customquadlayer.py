3f2c951da7b9468157f7f34248e89130a2f05d4c
col-a-guo <collinguo.d20@gmail.com>
2024-10-20 21:36:23 -0700

    added resampler, fixed misc metrics

--- customquadlayer.py
+++ customquadlayer.py
@@ -24,6 +24,7 @@ from sklearn.ensemble import StackingRegressor
 from sklearn.ensemble import StackingClassifier
 from sklearn.model_selection import train_test_split
 from sklearn import compose, linear_model, metrics, pipeline, preprocessing
+from imblearn.over_sampling import RandomOverSampler
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -54,10 +55,6 @@ data = data.dropna()
 
 scaler = MinMaxScaler()
 
-# Fit and transform the data
-data = pd.DataFrame(scaler.fit_transform(data), columns=data.columns)
-
-print(data)
 
 y = data.loc[:,"stage"].astype(int)
 
@@ -66,54 +63,70 @@ y.fillna(0)
 X = data[["scarcity", "nonuniform_progress", "performance_constraints", 
 "user heterogeneity", "cognitive", "external", "internal", "coordination", "technical", "demand", "paragraph"]]
 
-
-#replace nans with means
-# Splitting arrays or matrices into random train and test subsets
-# i.e. 70 % training dataset and 30 % test datasets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20)
+# Fit and transform the data
+X = pd.DataFrame(scaler.fit_transform(X), columns=X.columns)
 
 
-estimators = [
-    ('rf', RandomForestClassifier(n_estimators=10, random_state=42)),
-    ('svr', make_pipeline(PolynomialFeatures(degree=2),
-                        MinMaxScaler(),
-                        LinearSVC(random_state=42)))
-]
-#mnlr = linear_model.LogisticRegression(penalty='l2')
- #multinomial logistic regression
-# poly = PolynomialFeatures(degree=2)
+# Begin averaging loop
+total_accuracy = 0
+loop_count = 100
+for randomloop in range(loop_count):
+    ros = RandomOverSampler(random_state=randomloop)
+    X, y = ros.fit_resample(X, y)
 
-# Construct a TransformedTargetRegressor using this pipeline
-# ** So far the set-up has been standard **
-# regr = compose.TransformedTargetRegressor(regressor=mnlr, transformer=poly)
-# # Training the model on the training dataset
-# # fit function is used to train the model using the training sets as parameters
+    print(y)
 
-clf = StackingClassifier(
-    estimators=estimators, final_estimator=linear_model.LogisticRegression(penalty='l2')
-)
+    #replace nans with means
+    # Splitting arrays or matrices into random train and test subsets
+    # i.e. 70 % training dataset and 30 % test datasets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20)
 
-clf.fit(X_train, y_train)
 
-# Calculate permutation importance
-result = permutation_importance(clf, X, y, n_repeats=10, random_state=42)
+    estimators = [
+        ('rf', RandomForestClassifier(n_estimators=10, random_state=randomloop)),
+        ('svr', make_pipeline(PolynomialFeatures(degree=2),
+                            MinMaxScaler(),
+                            LinearSVC(random_state=42)))
+    ]
+    #mnlr = linear_model.LogisticRegression(penalty='l2')
+    #multinomial logistic regression
+    # poly = PolynomialFeatures(degree=2)
 
-# Access the importance scores
-importances = result.importances_mean
+    # Construct a TransformedTargetRegressor using this pipeline
+    # ** So far the set-up has been standard **
+    # regr = compose.TransformedTargetRegressor(regressor=mnlr, transformer=poly)
+    # # Training the model on the training dataset
+    # # fit function is used to train the model using the training sets as parameters
 
-# Print the importance scores
-for i, feature in enumerate(["scarcity", "nonuniform_progress", "performance_constraints", 
-"user heterogeneity", "cognitive", "external", "internal", "coordination", "technical", "demand", "paragraph"]):
-    print(f"{feature}: {importances[i]}")
+    clf = StackingClassifier(
+        estimators=estimators, final_estimator=linear_model.LogisticRegression(penalty='l2')
+    )
 
-# z_train = y_train
-# z_test = y_test
+    clf.fit(X_train, y_train)
 
-# clf2 = LogisticRegression(penalty='l2')
+    # Calculate permutation importance
+    result = permutation_importance(clf, X, y, n_repeats=10, random_state=42)
 
-# clf2.fit(clf.predict(poly_X_train).reshape(-1,1), z_train.reshape(-1,1))
- 
-# performing predictions on the test dataset
-y_pred = clf.predict(X_test)
+    # Access the importance scores
+    importances = result.importances_mean
 
-print("ACCURACY OF THE MODEL:", metrics.classification_report(y_test, y_pred))
+    # Print the importance scores
+    for i, feature in enumerate(["scarcity", "nonuniform_progress", "performance_constraints", 
+    "user heterogeneity", "cognitive", "external", "internal", "coordination", "technical", "demand", "paragraph"]):
+        print(f"{feature}: {importances[i]}")
+
+    # z_train = y_train
+    # z_test = y_test
+
+    # clf2 = LogisticRegression(penalty='l2')
+
+    # clf2.fit(clf.predict(poly_X_train).reshape(-1,1), z_train.reshape(-1,1))
+    
+    # performing predictions on the test dataset
+    y_pred = clf.predict(X_test)
+
+    print(metrics.classification_report(y_test, y_pred))
+    total_accuracy+=metrics.accuracy_score(y_test, y_pred)
+
+print("MEAN ACCURACY")
+print(total_accuracy/loop_count)
