3c9468a0500f11232dff45a86dfee3ec1eb46a17
col-a-guo <collinguo.d20@gmail.com>
2024-10-23 13:58:40 -0700

    fixed ratios, graphing

--- customquadlayer.py
+++ customquadlayer.py
@@ -29,6 +29,7 @@ from imblearn.under_sampling import RandomUnderSampler
 import matplotlib.pyplot as plt
 import numpy as np
 
+import matplotlib.pyplot as plt
 class MyStackingRegressor(StackingRegressor):
 
     # overwrite the inherited method without the check
@@ -67,6 +68,8 @@ X = data[["scarcity", "nonuniform_progress", "performance_constraints",
 # Fit and transform the data
 X = pd.DataFrame(scaler.fit_transform(X), columns=X.columns)
 
+#["scarcity", "nonuniform_progress", "performance_constraints", "user heterogeneity", "cognitive", "external", "internal", "coordination", "technical", "demand", "paragraph"]
+importance_array = [[] for i in range(11)]
 
 # Begin averaging loop
 acc_array = []
@@ -113,7 +116,7 @@ for randomloop in range(loop_count):
     # Print the importance scores
     for i, feature in enumerate(["scarcity", "nonuniform_progress", "performance_constraints", 
     "user heterogeneity", "cognitive", "external", "internal", "coordination", "technical", "demand", "paragraph"]):
-        print(f"{feature}: {importances[i]}")
+        importance_array[i].append(importances[i])
 
     # z_train = y_train
     # z_test = y_test
@@ -122,20 +125,36 @@ for randomloop in range(loop_count):
 
     # clf2.fit(clf.predict(poly_X_train).reshape(-1,1), z_train.reshape(-1,1))
 
-    rus = RandomUnderSampler(sampling_strategy =  {0: 40, 1: 5, 2: 5})
+    rus = RandomUnderSampler(sampling_strategy =  {0: 60, 1: 10, 2: 16})
     X_test, y_test = rus.fit_resample(X_test, y_test)
 
     # performing predictions on the test dataset
     y_pred = clf.predict(X_test)
 
-
-    print(metrics.classification_report(y_test, y_pred))
     acc_array.append(metrics.accuracy_score(y_test, y_pred))
+    if randomloop%10 == 0: 
+        print("loop "+str(randomloop)+" done")
 
-print("MEAN ACCURACY")
-print(np.mean(acc_array))
-print("STDEV:")
-print(np.std(acc_array))
+print(metrics.classification_report(y_test,y_pred))
+
+mean_acc = np.mean(acc_array)
+stdev_acc = np.std(acc_array)
+
+x= [i for i in range(11)]
+y_std = [np.std(implist) for implist in importance_array]
+y= [np.mean(implist) for implist in importance_array]
+
+plt.figure(figsize=(20, 8))
+plt.bar(x,y)
+plt.suptitle("Feature Importances for Random Forest -> Quadratic -> Logistic Regression")
+plt.title(f"Note: Accuracy averaged {str(mean_acc)[:4]} with stdev {str(stdev_acc)[:4]}")
+plt.xlabel("Feature")
+plt.ylabel("Importance")
+plt.errorbar(x, y, y_std, fmt='.', color='Black', elinewidth=2,capthick=10,errorevery=1, alpha=0.5, ms=4, capsize = 2)
+plt.xticks([i for i in range(11)], ["scarcity", "nonuniform\nprogress", "performance\nconstraints", 
+                                    "user\nheterogeneity", "cognitive", "external", "internal", "coordination", "technical", "demand", "paragraph"])
+
+plt.show()
 
 #Note to self: Non-oversampled version was 72% after 30 runs
 #New version is 77.5%, stdev 6.1% (100 runs)
