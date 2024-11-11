a9e6393aa1ec24aa44c28d9d0bf559178b651cae
Collin A Guo <bunjuku8@gmail.com>
2024-11-10 16:52:15 -0800

    fix BERT

--- BERTearnings.py
+++ BERTearnings.py
@@ -4,34 +4,30 @@ import numpy as np
 import evaluate
 import torch
 
-# Load the tokenizer
 tokenizer = AutoTokenizer.from_pretrained("pborchert/BusinessBERT")
 
 ogpath = "multichannel.csv"  
-# Load the dataset
 dataset = load_dataset('csv', data_files={'train': "train_" + ogpath, 'test': "test_" + ogpath})
 
-# Function to tokenize the text
+
 def tokenize_function(examples):
-    return tokenizer(examples["paragraph"], padding="max_length", truncation=True)
+    return tokenizer(examples["paragraph"], padding="max_length", truncation=True, max_length=512)
 
-# Tokenize the datasets
-tokenized_datasets = dataset.map(tokenize_function, batched=True)
 
-# Shuffle and select a small subset of the dataset
+tokenized_datasets = dataset.map(tokenize_function, batched=True, batch_size=16)
+
+
 small_train_dataset = tokenized_datasets["train"]
 small_eval_dataset = tokenized_datasets["test"]
 
-# Load the model for regression (change num_labels to 1 for regression)
-model = AutoModelForSequenceClassification.from_pretrained("pborchert/BusinessBERT", num_labels=1)
+# Load the model for regression (num_labels 1 for regression)
+model = AutoModelForSequenceClassification.from_pretrained("pborchert/BusinessBERT", num_labels=3)
 
-# Define training arguments
 training_args = TrainingArguments(
     output_dir="test_trainer",
     evaluation_strategy="epoch",
 )
 
-# Load evaluation metric (mean squared error for regression)
 metric = evaluate.load("mse")
 
 # Function to compute metrics
@@ -40,7 +36,6 @@ def compute_metrics(eval_pred):
     predictions = logits.squeeze()  # For regression, squeeze to remove unnecessary dimensions
     return metric.compute(predictions=predictions, references=labels)
 
-# Create Trainer
 trainer = Trainer(
     model=model,
     args=training_args,
@@ -49,5 +44,4 @@ trainer = Trainer(
     compute_metrics=compute_metrics,
 )
 
-# Train the model
 trainer.train()
